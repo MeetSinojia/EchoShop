3905e05eac1651c5e87fb1942b89aad8140994cf
Meet <sinojiameet321@gmail.com>
2023-06-15 02:42:03 +0530

    netlify

--- src/component/axios.js
+++ src/component/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // THE API (cloud function) URL
-    baseURL: '/.netlify/functions/api' // Update the baseURL to the Netlify Functions API endpoint
+    baseURL: '/.netlify/functions/payment-fetch' // Update the baseURL to the Netlify Functions API endpoint
 });
 
 export default instance;
